e95d383f5c26d426cc0d71178aa6c8acd4f07808
gyurcigyurma <gyurcigyurma@users.noreply.github.com>
2018-09-29 00:41:49 +0200

    Burger is now showing but not dynamically

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
+
+const burger = props => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top' />
+            <BurgerIngredient type='cheese' />
+            <BurgerIngredient type='meat' />
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+    );
+};
+
+burger.propTypes = {
+    
+};
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Aux from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
 
 export default class BurgerBuiler extends Component {
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger />
                 <div>Controls</div>
             </Aux>
         )
