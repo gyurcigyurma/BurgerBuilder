05b871d47f59067ac8eba5ca76893c7e3d9161f8
Gyorgy Raffay <gyurxi@gmail.com>
2018-09-28 15:55:12 +0200

    Initial commit for BurgerBuiler

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Auxi';
+
+export default class BurgerBuiler extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Controls</div>
+            </Aux>
+        )
+    }
+}
