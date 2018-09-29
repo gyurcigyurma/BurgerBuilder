0e33a70406c7c176e3409211371ab3682e079f26
gyurcigyurma <gyurcigyurma@users.noreply.github.com>
2018-09-30 00:36:02 +0200

    Ingredient now dynamic but only setable from code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Aux from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 
 export default class BurgerBuiler extends Component {
+    /*constructor (props) {
+        super(props);        
+    }*/
+
+    state = {
+        ingredients : {
+            salad : 0,
+            bacon : 0,
+            cheese : 0,
+            meat: 0
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients = {this.state.ingredients} />
                 <div>Controls</div>
             </Aux>
         )
